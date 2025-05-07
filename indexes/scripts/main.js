bf6427c0a947d489db7c3848125db1905b822dcf
cypriotCodder <huraynedim@icloud.com>
2025-05-08 01:03:58 +0300

    Add responsive styles, update navigation links, and create training page

--- indexes/scripts/main.js
+++ indexes/scripts/main.js
@@ -3,3 +3,13 @@ const toggle = document.querySelector('.nav-toggle');
     toggle.addEventListener('click', () => {
       links.style.display = links.style.display === 'block' ? 'none' : 'block';
     });
+
+document.getElementById("strength").addEventListener("click", () => {
+    console.log("Strength button clicked!");
+    window.location.href = "training.html";
+})
+
+document.getElementById("home").addEventListener("click", () => {
+    window.location.href = "home.html";   
+    console.log("Home button clicked!");
+})
