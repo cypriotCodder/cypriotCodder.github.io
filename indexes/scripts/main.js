4dfef5aa665483e297a745518854232fc24af9e0
cypriotCodder <huraynedim@icloud.com>
2025-05-08 11:55:00 +0300

    Refactor navigation links for consistency, enhance button styles, and improve transition effects

--- indexes/scripts/main.js
+++ indexes/scripts/main.js
@@ -1,15 +1,18 @@
 const toggle = document.querySelector('.nav-toggle');
-    const links = document.querySelector('.nav-links');
-    toggle.addEventListener('click', () => {
-      links.style.display = links.style.display === 'block' ? 'none' : 'block';
-    });
+const links = document.querySelector('.nav-links');
+toggle.addEventListener('click', () => {
+  links.style.display = links.style.display === 'block' ? 'none' : 'block';
+});
 
-document.getElementById("strength").addEventListener("click", () => {
+window.addEventListener("DOMContentLoaded", () => {
+
+  document.getElementById("strength").addEventListener("click", () => {
+  window.location.href = "./training.html";
     console.log("Strength button clicked!");
-    window.location.href = "training.html";
-})
+  })
 
-document.getElementById("home").addEventListener("click", () => {
-    window.location.href = "home.html";   
-    console.log("Home button clicked!");
-})
+  document.getElementsByClassName("btngeri").addEventListener("click", () => {
+    window.location.href = "./home.html";   
+    console.log("geri button clicked!");
+  });
+});
