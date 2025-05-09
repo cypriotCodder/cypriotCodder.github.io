08739018afb2169235f95b3263a04f8580a5ea17
nedim <huraynedim@icloud.com>
2025-05-09 15:53:35 +0300

    gifs fixed--back

--- indexes/scripts/main.js
+++ indexes/scripts/main.js
@@ -8,6 +8,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const strength = document.getElementById('strength');
   const backbtn  = document.getElementById('backbtn');
+  //muscle parts
+  const back = document.getElementById('backPart');
+
   if (strength) {
     strength.addEventListener('click', () => {
       window.location.href = 'training.html';
@@ -18,41 +21,16 @@ window.addEventListener('DOMContentLoaded', () => {
       window.location.href = 'home.html';
     });
   }
+  const gifs = document.querySelector('.gifs');
+  if(gifs && back){
+    back.addEventListener('click', () =>{
+      gifs.classList.toggle('hidden')
+      console.log("clicked")
+    })
+  }
+
 });
 
 const sirtLink     = document.getElementById('sirtLink');
 const programsSec  = document.getElementById('gif1');
-// 2. Define your GIF URLs
-const sirtGifs = [
-  '../assets/back.gif',
-  '../assets/back2.gif',
 
-  
-  // …add as many as you like
-];
-
-// sirtLink.addEventListener('click', e => {
-//   e.preventDefault();               // don’t reload the page
-//   programsSec.innerHTML = '';       // clear out any old content
-//   // 3. Create and append <img> for each GIF
-//   sirtGifs.forEach(src => {
-//     const img = document.createElement('img');
-//     img.src = src;
-//     img.alt = 'Sırt programı demo';
-//     img.classList.add('program-gif');
-//     programsSec.appendChild(img);
-//   });
-// });
-
-// Grab all toggle-buttons
-document.querySelectorAll('a[data-target]').forEach(btn => {
-  btn.addEventListener('click', () => {
-    // Get the ID of the section we want to toggle
-    const sectionId = btn.getAttribute('data-target');
-    const section = document.getElementById(sectionId);
-    if (!section) return;
-
-    // Toggle the "hidden" class
-    section.classList.toggle('hidden');
-  });
-});
