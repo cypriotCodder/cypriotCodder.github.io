f478e1513db734a1c29df56fe3cf91869347ebf3
cypriotCodder <huraynedim@icloud.com>
2025-05-12 09:32:58 +0300

    Refactor code structure for improved readability and maintainability

--- indexes/scripts/main.js
+++ indexes/scripts/main.js
@@ -8,8 +8,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const strength = document.getElementById('strength');
   const backbtn  = document.getElementById('backbtn');
+
   //muscle parts
   const back = document.getElementById('backPart');
+  const chest = document.getElementById('chestPart');
+  const arms = document.getElementById('armsPart');
+  const legs = document.getElementById('legsPart');
+  const abs = document.getElementById('absPart');
+  const glutes = document.getElementById('glutesPart');
+  const shoulders = document.getElementById('shouldersPart');
 
   if (strength) {
     strength.addEventListener('click', () => {
@@ -21,10 +28,25 @@ window.addEventListener('DOMContentLoaded', () => {
       window.location.href = 'home.html';
     });
   }
-  const gifs = document.querySelector('.gifs');
-  if(gifs && back){
+  const backgifs = document.querySelector('#backGifs');
+  const chestgifs = document.querySelector('#chestGifs');
+  const armsgifs = document.querySelector('#armGifs');
+
+  if(backgifs){
     back.addEventListener('click', () =>{
-      gifs.classList.toggle('hidden')
+      backgifs.classList.toggle('hidden')
+      console.log("clicked")
+    })
+  }
+  if(chestgifs){
+    chest.addEventListener('click', () =>{
+      chestgifs.classList.toggle('hidden')
+      console.log("clicked")
+    })
+  }
+  if(armsgifs){
+    armsgifs.addEventListener('click', () =>{
+      armsgifs.classList.toggle('hidden')
       console.log("clicked")
     })
   }
