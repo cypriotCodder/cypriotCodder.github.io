71a0aa2d1cee4789828eb75d9d6ef6fa76a084ce
cypriotCodder <huraynedim@icloud.com>
2025-05-23 23:41:09 +0300

    Add "Scroll to Top" button functionality and styles

--- indexes/scripts/main.js
+++ indexes/scripts/main.js
@@ -118,4 +118,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
 });
 
+//scroll to top button
+const goTopBtn = document.getElementById('goTopBtn');
 
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+      goTopBtn.style.display = 'block';
+    } else {
+      goTopBtn.style.display = 'none';
+    }
+  });
+
+  goTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
