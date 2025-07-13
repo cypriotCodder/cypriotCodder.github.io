1eb793756dbb04406a6a26b603732d5f583bf019
cypriotCodder <huraynedim@icloud.com>
2025-07-13 14:09:13 +0300

    1

--- indexes/scripts/main.js
+++ indexes/scripts/main.js
@@ -36,10 +36,16 @@ window.addEventListener('DOMContentLoaded', () => {
   //buttons
   const strength = document.getElementById('strength');
   const backbtn  = document.getElementById('backbtn');
+  const programs = document.getElementById('programs');
 
   if (strength) {
     strength.addEventListener('click', () => {
       window.location.href = 'training.html';
+    });
+  }
+  if(programs){
+    programs.addEventListener('click', () => {
+      window.location.href = 'programme.html';
     });
   }
   if (backbtn) {
@@ -55,30 +61,6 @@ window.addEventListener('DOMContentLoaded', () => {
       window.location.href = 'home.html';
     });
   }
-
-
-  //muscle parts
-  const back = document.getElementById('backPart');
-  const chest = document.getElementById('chestPart');
-  const shoulders = document.getElementById('shoulderPart');
-  const arms = document.getElementById('armsPart');
-  const legs = document.getElementById('legsPart');
-  const abs = document.getElementById('absPart');
-  const glutes = document.getElementById('glutePart');
-
-  //gif sections
-  const backgifs = document.querySelector('#backGifs');
-  const chestgifs = document.querySelector('#chestGifs');
-  const armsgifs = document.querySelector('#armGifs');
-  const shouldergifs = document.querySelector("#shoulderGifs");
-  const leggifs = document.querySelector("#legGifs");
-  const glutegifs = document.querySelector("#gluteGifs");
-  const absgifs = document.querySelector("#absGifs");
-  const cardiogifs = document.querySelector("#cardioGifs");
-
-  //gif sections array
-  const gifSections = [];
-  gifSections.push(backgifs, chestgifs, armsgifs, shouldergifs, absgifs, cardiogifs, glutegifs, leggifs);
 
   // const hash = window.location.hash;
   // if (hash) {
@@ -96,79 +78,7 @@ window.addEventListener('DOMContentLoaded', () => {
     
   // }
 
-  // // toggle gifs by body part
-  //   if(backgifs) {
-  //     back.addEventListener('click', () =>{
-  //     backgifs.classList.toggle('hidden');
-  //     toggleOff(backgifs, gifSections);
-  //     console.log("clicked");
-
-  //     if(window.innerWidth <= 768)
-  //       bodyPartsSideBarToggle()
-  //   })
-  //   }
-  //   if(chestgifs) {
-  //     chest.addEventListener('click', () =>{
-  //       chestgifs.classList.toggle('hidden');
-  //       toggleOff(chestgifs, gifSections);
-  //       console.log("clicked");
-
-  //       if(window.innerWidth <= 768)
-  //         bodyPartsSideBarToggle()
-  //     })
-  // } 
-  //   if(shouldergifs) {
-  //     shoulders.addEventListener('click', () =>{
-  //     shouldergifs.classList.toggle('hidden');
-  //     toggleOff(shouldergifs, gifSections);
-  //     console.log("clicked");
-
-  //     if(window.innerWidth <= 768)
-  //       bodyPartsSideBarToggle()
-  //   })
-  //   }
-  //   if(armsgifs) {
-  //     arms.addEventListener('click', () =>{
-  //     armsgifs.classList.toggle('hidden');
-  //     toggleOff(armsgifs, gifSections);
-  //     console.log("clicked");
-
-  //     if(window.innerWidth <= 768)
-  //       bodyPartsSideBarToggle()
-  //   })
-  //   }
-  //   if(leggifs) {
-  //     legs.addEventListener('click', () =>{
-  //     leggifs.classList.toggle('hidden');
-  //     toggleOff(leggifs, gifSections);
-  //     console.log("clicked");
-
-  //     if(window.innerWidth <= 768)
-  //       bodyPartsSideBarToggle()
-  //   })
-  //   }
-  //   if(absgifs) {
-  //     abs.addEventListener('click', () =>{
-  //     absgifs.classList.toggle('hidden');
-  //     toggleOff(absgifs, gifSections);
-  //     console.log("clicked");
-
-  //     if(window.innerWidth <= 768)
-  //       bodyPartsSideBarToggle()
-  //   })
-  //   }
-  //   if(glutegifs) {
-  //     glutes.addEventListener('click', () =>{
-  //     glutegifs.classList.toggle('hidden');
-  //     toggleOff(glutegifs, gifSections);
-  //     console.log("clicked");
-
-  //     if(window.innerWidth <= 768)
-  //       bodyPartsSideBarToggle()
-  //   })
-  //   }
-    
-  //TODO cardio  
+  
 
   const coll = document.querySelector('.sidebar');
   const title = coll.querySelector('.sidebar__title');
